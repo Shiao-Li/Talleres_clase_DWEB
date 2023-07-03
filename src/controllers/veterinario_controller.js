e6eb6a03188d7359c775da09744a69b3442ebcb6
Roberto Shiao <95731527+Shiao-Li@users.noreply.github.com>
2023-07-03 16:46:11 -0500

    Add files via upload

---
+++ src/controllers/veterinario_controller.js
@@ -0,0 +1,115 @@
+// Importo el modelo veterinario
+import Veterinario from "../models/Veterinario.js"
+import sendMailToUser from "../config/nodemailer.js"
+
+// Todas las funciones
+
+// METODO PARA LOGIN
+const login = async(req,res)=>{
+    // Capturar datos del requests
+    const {email,password} = req.body
+    // Validacion de campos vacios
+    if (Object.values(req.body).includes("")) return res.status(404).json({msg:"Lo sentimos, debes llenar todos los campos"})
+    // Obtener el usuario en base al email
+    const veterinarioBDD = await Veterinario.findOne({email}).select("-status -__v -token -updatedAt -createdAt")
+    // Validacion de la cuneta del email
+    if(veterinarioBDD?.confirmEmail===false) return res.status(403).json({msg:"Lo sentimos, debe verificar su cuenta"})
+    // Validar su existe el usuario
+    if(!veterinarioBDD) return res.status(404).json({msg:"Lo sentimos, el usuario no se encuentra registrado"})
+    // Validar si el password del reques es el mismo de la bdd
+    const verificarPassword = await veterinarioBDD.matchPassword(password)
+    if(!verificarPassword) return res.status(404).json({msg:"Lo sentimos, el password no es el correcto"})
+    // Desesetructurar la info del usario
+    const {nombre,apellido,direccion,telefono,_id} = veterinarioBDD
+    // Presentar datos
+    res.status(200).json({
+        nombre,
+        apellido,
+        direccion,
+        telefono,
+        _id,
+        email:veterinarioBDD.email
+    })
+}
+
+const perfil=(req,res)=>{
+    res.status(200).json({res:'perfil del veterinario'})
+}
+
+// METODO PARA EL REGISTRO
+const registro = async (req,res)=>{
+    // Capturar los datos del boy de la peticion
+    const {email,password} = req.body
+    // Validacion de los campos vacios 
+    if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
+    // Verificar la existencia del mail
+    const verificarEmailBDD = await Veterinario.findOne({email})
+    if(verificarEmailBDD) return res.status(400).json({msg:"Lo sentimos, el email ya se encuentra registrado"})
+    // Crear la instancia del modelo
+    const nuevoVeterinario = new Veterinario(req.body)
+    // Encriptar el assword del usuario
+    nuevoVeterinario.password = await nuevoVeterinario.encrypPassword(password)
+    // Crear el token del usuario
+    const token = nuevoVeterinario.crearToken()
+    // Invocar la funcion para el envio del correo
+    await sendMailToUser(email,token)
+    // Guardar en la BDD
+    await nuevoVeterinario.save()
+    // Enviar la respuesta
+    res.status(200).json({msg:"Revisa tu correo electrónico para confirmar tu cuenta"})
+}
+
+// METODO PARA LA CONFIRMACION DEL USUARIO
+const confirmEmail = async (req,res)=>{
+    // Validar el token del correo
+    if(!(req.params.token)) return res.status(400).json({msg:"Lo sentimos, no se puede validar la cuenta"})
+    // Verificar si en base al token existe ese usuario
+    const veterinarioBDD = await Veterinario.findOne({token:req.params.token})
+    // Validar si el token ya fue seteado a null
+    if(!veterinarioBDD?.token) return res.status(404).json({msg:"La cuenta ya ha sido confirmada"})
+    // setear a null el token
+    veterinarioBDD.token = null
+    // Cambiar a true la confirmacion de la cuenta
+    veterinarioBDD.confirmEmail=true
+    // Guarda cambios en la BDD
+    await veterinarioBDD.save()
+    // Presentar el mensaje al usuario
+    res.status(200).json({msg:"Token confirmado, ya puedes iniciar sesión"}) 
+}
+
+const listarVeterinarios = (req,res)=>{
+    res.status(200).json({res:'lista de veterinarios registrados'})
+}
+const detalleVeterinario = (req,res)=>{
+    res.status(200).json({res:'detalle de un eterinario registrado'})
+}
+const actualizarPerfil = (req,res)=>{
+    res.status(200).json({res:'actualizar perfil de un veterinario registrado'})
+}
+const actualizarPassword = (req,res)=>{
+    res.status(200).json({res:'actualizar password de un veterinario registrado'})
+}
+const recuperarPassword= (req,res)=>{
+    res.status(200).json({res:'enviar mail recuperación'})
+}
+const comprobarTokenPasword= (req,res)=>{
+    res.status(200).json({res:'verificar token mail'})
+}
+const nuevoPassword= (req,res)=>{
+    res.status(200).json({res:'crear nuevo password'})
+}
+
+// Exportacio nombrada
+export {
+    login,
+    perfil,
+    registro,
+    confirmEmail,
+    listarVeterinarios,
+    detalleVeterinario,
+    actualizarPerfil,
+    actualizarPassword,
+	recuperarPassword,
+    comprobarTokenPasword,
+	nuevoPassword
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Importacion de la varieble app por medio de modulos
+import app from './server.js'
+import connection from './database.js';
+
+connection()
+
+// Ejecutar el servidor por medio del puerto 3000
+app.listen(app.get('port'),()=>{
+    console.log(`Server ok on http://localhost:${app.get('port')}`);
+})
